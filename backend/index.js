9f629e76f1ed1bb3b7526afca7adcca71e011cdc
Akshay <akshayhegde158@gmail.com>
2021-08-05 00:19:46 +0530

    mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express=require("express");
+const mongoose = require("mongoose")
+const mongodb = "mongodb+srv://admin:admin123@cluster0.abkev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const app=express()
 let PORT =process.env.PORT||5000
+mongoose.connect(mongodb,{useNewUrlParser: true,  useUnifiedTopology: true  } ).then(()=>{app.listen(PORT,()=>{
+    console.log(`Listening at ${PORT}`)
+})})
 
 app.get("",(req,res)=>{
     res.send("hey")
-})
-app.listen(PORT,()=>{
-    console.log(`Listening at ${PORT}`)
 })
