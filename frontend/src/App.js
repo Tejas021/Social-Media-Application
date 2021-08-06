74b61306b54f7842749bd37a3c742390741340e9
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2021-08-06 11:20:17 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/utilities/Navbar"
-import Home from "./components/home/Home"
-import Login from "./components/login/Login"
-import Signup from "./components/login/Signup"
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home />
-      <Login />
-      <Signup />
+    
  
     </div>
   );
