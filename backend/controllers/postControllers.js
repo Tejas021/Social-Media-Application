106bbd12b7f261a898209a4f1433c849c9c81c63
Akshay <akshayhegde158@gmail.com>
2021-10-07 12:09:33 +0530

    like

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -3,8 +3,8 @@ const Post = require("../models/Post")
 
 module.exports.addPost=async(req,res)=>{
     console.log(req.body)
-    const {content,caption,like,user,comments,imgUrl,userId}=req.body
-    const np=new Post({content:content,like:like,caption:caption,user:user,comments:comments,imgUrl:imgUrl,userId:userId})
+    const {content,caption,like,user,comments,likedPeople,imgUrl,userId}=req.body
+    const np=new Post({content:content,like:like,caption:caption,user:user,comments:comments,likedPeople:likedPeople,imgUrl:imgUrl,userId:userId})
     await np.save().then((newpost)=>res.status(201).send(newpost));
    
 
@@ -26,17 +26,63 @@ module.exports.deletePost=()=>{
 
 }
 
-module.exports.handleLike=async(req,res)=>{
+// module.exports.handleLike=async(req,res)=>{
 
-  
+//   const verUser = await Post.exists({likedPeople:req.body.friend_id})
+//   console.log(verUser)
+//   if (!verUser){
+//     console.log("new like")
+//       if(req.body.liked){
+        
+//         const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 1},$push: { likedPeople: req.body.friend_id }} ,{new: true, useFindAndModify: false})
+//         console.log("called")
+//         return res.status(200).send(post)
+//       }
+//       else{
+//         const post= await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : -1}}, {new: true, useFindAndModify: false})
+        
+//           return res.status(200).send(post)
+//       }
+//   } else{
+//     console.log("warning same person ")
+//     if(req.body.liked){
+        
+//       const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 0},$push: { likedPeople: req.body.friend_id }} ,{new: true, useFindAndModify: false})
+//       console.log("called")
+//       return res.status(200).send(post)
+//     }
+//     else{
+//       const post= await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 0}}, {new: true, useFindAndModify: false})
+      
+//         return res.status(200).send(post)
+//     }
+//   }
+// }
+
+module.exports.handleLike=async(req,res)=>{
+  const verLikedUser = await Post.exists({likedPeople:req.body.friend_id})
+  const verDislikedUser = await Post.exists({dislikedPeople:req.body.friend_id})
+ 
   if(req.body.liked){
-    const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 1}} ,{new: true, useFindAndModify: false})
-console.log("called")
-    return res.status(200).send(post)
+      if(!verLikedUser){
+        console.log("new like")
+        const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 1},$push: { likedPeople: req.body.friend_id }} ,{new: true, useFindAndModify: false})
+        return res.status(200).send(post)
+      } else {
+        console.log("same user liked")
+        const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 0}} ,{new: true, useFindAndModify: false})
+        return res.status(200).send(post)
+      }
+  } else {
+      if((verLikedUser) && (!verDislikedUser)){
+        console.log("new dislike")
+        const post= await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : -1}, $push: { dislikedPeople: req.body.friend_id }},{new: true, useFindAndModify: false})
+        return res.status(200).send(post)
+      } else{
+        console.log("cannot dislike")
+        const post=await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : 0}} ,{new: true, useFindAndModify: false})
+        return res.status(200).send(post)
+      }
   }
-  else{
-     const post= await Post.findByIdAndUpdate(req.body.post_id,{$inc : {'like' : -1}}, {new: true, useFindAndModify: false})
-     
-      return res.status(200).send(post)
-  }
+
 }
--- frontend/src/components/home/Post.js
+++ frontend/src/components/home/Post.js
@@ -2,35 +2,55 @@ import React from 'react'
 import "./Home.css"
 import {Link} from "react-router-dom"
 import {useState} from "react"
-const Post = ({Name,Caption,likes,img,post_id,user_id}) => {
 
+
+const Post = ({Name,Caption,likes,img,post_id,user_id,friend_id}) => {
+
+ 
     const [liked,setLiked]=useState(true)
+    const [likeCount,setLikeCount]=useState(likes)
+    const [fillLike,setFillLike]=useState(false)
    
-
+   
     const handleLike=(e)=>{
         e.preventDefault();
         setLiked(!liked)
         if(liked){
-            likes=likes+1
+            console.log("like")
+            
+            setFillLike(true)
+            console.log(fillLike)
+            
         }
         else{
-            likes=likes-1
+            console.log("unlike")
+           
+            setFillLike(false)
+            console.log(fillLike)
+            
         }
-        let data={post_id,liked}
+        let data={post_id,liked,friend_id}
         fetch("http://localhost:5000/like",{
             method:"POST",
             headers:{"Content-type":"application/json"},
             body:JSON.stringify(data)
-        }).then(res=>res.json()).catch(err=>console.log(err))
+        }).then(res=>res.json()).then(r=>{
+            console.log(r)
+            setLikeCount(r.like)
+        })
+        .catch(err=>console.log(err))
     }
+    // console.log(likeCount)
     return (
         <div className="postbox mx-2 my-4 py-2">
         <div className="uname"><h5><i className="far fa-user-circle"></i><Link className="name_link" to={`/profile/${user_id}`}> {Name}</Link> </h5></div>
         <img className="my-2 postimage" src={img} alt=""/>
         <div className="caption m-1">{Caption}</div>
                 <div className="row like mt-3">
-                   <div className="col-lg-5"><i className="far fa-thumbs-up" onClick={e=>handleLike(e)}></i><span ><p>{likes}</p></span></div> 
-                   
+                      { !fillLike ?  
+                   <div className="col-lg-5"><i className="far fa-thumbs-up" onClick={e=>handleLike(e)}></i><span ><p>{likeCount}</p></span></div> 
+                   : <div className="col-lg-5"><i className="fas fa-thumbs-up" onClick={e=>handleLike(e)}></i><span ><p>{likeCount}</p></span></div> 
+                     } 
                     <div className="col-lg-3"><i className="far fa-comment  "></i></div>
                 </div>
    </div>
