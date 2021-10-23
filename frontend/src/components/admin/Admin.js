2c9f4f1aec351a48a49fead5c2d7cdbc644c0478
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2021-10-23 23:05:15 +0530

    added images

---
+++ frontend/src/components/admin/Admin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Admin = () => {
+    return (
+        <div>
+            <h1 className="text-warning">Welcome To The Admin Panel</h1>
+            <form>
+                <input/>
+                <input/>
+                <button class="btn btn-warning">ADD EVENT</button>
+            </form>
+        </div>
+    )
+}
+
+export default Admin
